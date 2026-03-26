6f3385fc2d9bb11aff8a0a0116152c1260f170fc
Mario Reder <mario.reder@pm.me>
2026-03-26 17:25:06 +0100

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -1,6 +1,6 @@
 window.__RUNTIME_CONFIG__ = {
   "VITE_ORDERLY_BROKER_ID": "demo",
-  "VITE_ORDERLY_BROKER_NAME": "czr",
+  "VITE_ORDERLY_BROKER_NAME": "czrdex",
   "VITE_BROKER_EOA_ADDRESS": "0x4a17d78c905a50606a7678b54c8bfba048391743",
   "VITE_DISABLE_MAINNET": "false",
   "VITE_DISABLE_TESTNET": "false",
@@ -14,7 +14,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_DISABLE_EVM_WALLETS": "false",
   "VITE_DISABLE_SOLANA_WALLETS": "false",
   "VITE_WALLETCONNECT_PROJECT_ID": "",
-  "VITE_APP_NAME": "czr",
+  "VITE_APP_NAME": "czrdex",
   "VITE_APP_DESCRIPTION": "Trade perpetuals on CZR, the top DEX with low fees, deep liquidity, multi-chain support, high leverage and yield.",
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "false",
